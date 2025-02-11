9606f5a91ba33aa57cdc2b13a0c501f570210d99
Gabriel Meiki <gabrielmaeda01@gmail.com>
2025-02-11 17:38:55 -0300

    CONEXAO COM API

---
+++ script.js
@@ -0,0 +1,33 @@
+// Conexão dos elementos com o DOM
+
+let primeiroUsuarioInput = document.querySelector("#primeiro-usuario");
+
+let segundoUsuarioInput = document.querySelector('#segundo-usuario');
+
+let btnSubmit = document.querySelector('#btnSubmit');
+
+let formulario = document.querySelector('#form-search-profile');
+
+
+let resultado = document.querySelector('#resultado-da-busca');
+
+
+async function mostrarResultado(){
+    let primeiroUsuario = primeiroUsuarioInput.value
+    let segundoUsuario = segundoUsuarioInput.value
+    let usuario1 = await buscarPerfil(`${primeiroUsuario}`);
+    let usuario2 = await buscarPerfil(`${segundoUsuario}`);
+
+    resultado.innerHTML = `<p> ${usuario1.login} e ${usuario2.name} </p>`;
+}
+
+async function buscarPerfil(usuarioNome) {
+    const resposta = await fetch(`https://api.github.com/users/${usuarioNome}`);
+    const usuarioInfo = await resposta.json();
+    return usuarioInfo;
+}
+
+
+
+
+
